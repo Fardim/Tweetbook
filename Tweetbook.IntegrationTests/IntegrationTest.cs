2be0a56b08c788436c368463924c802ca72cf153
FARDIM KAISER <fardim_drmc_9094@yahoo.com>
2020-03-12 09:34:16 +0600

    integration testing done

--- Tweetbook.IntegrationTests/IntegrationTest.cs
+++ Tweetbook.IntegrationTests/IntegrationTest.cs
@@ -2,17 +2,63 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Tweetbook.Contracts;
+using Tweetbook.Contracts.V1.Requests;
+using Tweetbook.Contracts.V1.Responses;
+using Tweetbook.Data;
 
 namespace Tweetbook.IntegrationTests
 {
-    public class IntegrationTest
+    public class IntegrationTest: IDisposable
     {
-        private readonly HttpClient TestClient;
-        public IntegrationTest()
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider _serviceProvider;
+        protected IntegrationTest()
         {
-            var appFactory = new WebApplicationFactory<Startup>();
+            var appFactory = new WebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.RemoveAll(typeof(DataContext));
+                    services.AddDbContext<DataContext>(options => { options.UseInMemoryDatabase("TestDb"); });
+                });
+            });
+            _serviceProvider = appFactory.Services;
             TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+            
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register,
+                new UserRegistrationRequest {Email = "fa@yopmail.com", Password = "1qazZAQ!"});
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DataContext>();
+            context.Database.EnsureDeleted();
         }
     }
 }
